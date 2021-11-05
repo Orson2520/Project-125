8635e4bbe1ed980e7f10f1299559165b6de37df2
Orson2520 <83204975+Orson2520@users.noreply.github.com>
2021-11-05 17:21:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,15 +20,6 @@ function modelLoaded()
     console.log('PoseNet Is Initialized!');
 }
 
-function draw()
-{
-    background('#6C91C2');
-    
-    textsize(difference);
-    fill('#FFE787');
-    text('Orson', 50, 400);
-}
-
 function gotPoses(results)
 {
     if(results.length > 0)
@@ -38,5 +29,16 @@ function gotPoses(results)
         leftWristX = results[0].pose.leftWrist.x;
         rightWristX = results[0].pose.rightWrist.x;
         difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
+}
+
+function draw()
+{
+    background('#6C91C2');
+
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px"; 
+    textSize(difference);    
+    fill('#FFE787');
+    text('Orson', 50, 400);
 }
